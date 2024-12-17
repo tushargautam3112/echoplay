80af89327cb6aa87e0f1637a06f15876fd74c0b4
Tushar-rg <77859244+Tushar-rg@users.noreply.github.com>
2024-12-17 18:17:14 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer);
+
+io.on("connect", (socket) => {
+  console.log("socket: ", socket.id)
+});
+
+app.use(express.static("public"));
+
+
+httpServer.listen(5000);
